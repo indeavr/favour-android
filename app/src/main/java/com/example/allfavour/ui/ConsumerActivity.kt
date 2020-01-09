397a23cbff1585816dd695417daafdef12a5a7f8
Donevski <donevskimartin@gmail.com>
2020-01-09 07:23:33 +0200

    colors & gradle update

---
+++ app/src/main/java/com/example/allfavour/ui/ConsumerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.allfavour.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.allfavour.R
+
+class ConsumerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consumer)
+    }
+}
