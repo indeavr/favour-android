95a7a1519728a080371881ef52f9fac2f75604a0
Donevski <donevskimartin@gmail.com>
2020-01-10 07:39:15 +0200

    build build.gradle improvementes + top menu done from a navigations menu

--- app/src/main/java/com/example/allfavour/ui/ConsumerActivity.kt
+++ app/src/main/java/com/example/allfavour/ui/ConsumerActivity.kt
@@ -3,17 +3,29 @@ package com.example.allfavour.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.allfavour.R
 
 class ConsumerActivity : AppCompatActivity() {
-    lateinit var toolbar: Toolbar
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consumer)
 
-        toolbar = findViewById(R.id.toolbar_consumer)
+        val toolbar = findViewById<Toolbar>(R.id.consumer_toolbar)
         setSupportActionBar(toolbar)
+
+        var hostContainer: NavHostFragment =supportFragmentManager
+            .findFragmentById(R.id.consumer_nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController: NavController = hostContainer.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+    }
+
+    fun setupBottomAppBar(navController: NavController){
+
     }
 }
