80dc313ee7b266c273e5c533cf6bbac17a5b9039
Donevski <donevskimartin@gmail.com>
2020-01-23 09:43:58 +0200

    Added BasicInfoForm

--- app/src/main/java/com/example/allfavour/ui/BasicInfoFormFragment.kt
+++ app/src/main/java/com/example/allfavour/ui/BasicInfoFormFragment.kt
@@ -6,45 +6,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import com.example.allfavour.MainNavigationDirections
 
 import com.example.allfavour.R
-import com.example.allfavour.utility.HandleNotifications
-import kotlinx.android.synthetic.main.basic_info_form_fragment.*
+import com.example.allfavour.databinding.BasicInfoFormFragmentBinding
+import androidx.lifecycle.Observer
+
 
 class BasicInfoFormFragment : Fragment() {
-
     val mainNavController: NavController? by lazy { activity?.findNavController(R.id.main_nav_activity) }
 
-    companion object {
-        fun newInstance() = BasicInfoFormFragment()
+    private val viewModel: BasicInfoFormViewModel by lazy {
+        ViewModelProviders.of(this).get(BasicInfoFormViewModel::class.java)
     }
-
-    private lateinit var viewModel: BasicInfoFormViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.basic_info_form_fragment, container, false)
+
+        val binding: BasicInfoFormFragmentBinding =
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.basic_info_form_fragment,
+                container,
+                false
+            )
+
+        viewModel.init()
+
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
+
+        setupSubmit()
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        basic_forms_submit_button.setOnClickListener {
+    private fun setupSubmit() {
+        viewModel.submittedSuccessfully.observe(this, Observer<Boolean> {
             val action = MainNavigationDirections.consumerSearchDest()
             mainNavController?.navigate(action)
-        }
+        })
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(BasicInfoFormViewModel::class.java)
-        // TODO: Use the ViewModel
+    companion object {
+        fun newInstance() = BasicInfoFormFragment()
     }
-
 }
--- app/src/main/java/com/example/allfavour/ui/BasicInfoFormViewModel.kt
+++ app/src/main/java/com/example/allfavour/ui/BasicInfoFormViewModel.kt
@@ -1,7 +1,51 @@
 package com.example.allfavour.ui
 
+import android.view.View
 import androidx.lifecycle.ViewModel
+import android.widget.EditText
+import androidx.lifecycle.MutableLiveData
+import androidx.databinding.BindingAdapter
+
 
 class BasicInfoFormViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    lateinit var fields: BasicFormModel
+
+    lateinit var onFocusPhoneNumber: View.OnFocusChangeListener
+    val submittedSuccessfully = MutableLiveData<Boolean>()
+
+    fun init() {
+        fields = BasicFormModel()
+        onFocusPhoneNumber = View.OnFocusChangeListener { view, focused ->
+            val et = view as EditText
+            if (et.text.isNotEmpty() && !focused) {
+                fields.isPhoneNumberValid(true)
+            }
+        }
+    }
+
+    fun onSubmit() {
+        if (fields.valid) {
+            // do graphql request then change value to navigate
+            submittedSuccessfully.value = true
+        }
+    }
+
 }
+
+@BindingAdapter("error")
+fun setError(editText: EditText, strOrResId: Any?) {
+    if (strOrResId is Int) {
+        editText.error = editText.context.getString(strOrResId)
+    } else if (strOrResId is String) {
+        editText.error = strOrResId
+    }
+
+}
+
+@BindingAdapter("onFocus")
+fun bindFocusChange(editText: EditText, onFocusChangeListener: View.OnFocusChangeListener) {
+    if (editText.onFocusChangeListener == null) {
+        editText.onFocusChangeListener = onFocusChangeListener
+    }
+}
+
